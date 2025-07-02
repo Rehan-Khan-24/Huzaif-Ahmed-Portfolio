e2751c1f142b72c1f13692d93451d9a86eaa75ce
Rehan-Khan-24 <rehanrise24@gmail.com>
2025-07-03 01:15:35 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,53 +1,7 @@
-import React, { useEffect } from "react";
-import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-const Tech = () => {
-  useEffect(() => {
-    gsap.fromTo(
-      ".tech-icon",  
-      {
-        opacity: 0,
-        y: 80
-      },
-      {
-
-
-
-        
-        opacity: 1,
-        y: 0,
-        duration: 2.5,
-        stagger: 0.1, 
-        scrollTrigger: {
-          trigger: ".tech-icons-wrapper", 
-          start: "top 80%", 
-          end: "bottom 70%", 
-          scrub: true, 
-        },
-      }
-    );
-  }, []);
-
-  return (
-    <section>
-      <div className="tech-icons-wrapper flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <img
-              src={technology.icon}
-              alt={technology.name}
-              className="tech-icon w-full h-full object-contain"
-            />
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default SectionWrapper(Tech, "");
+export const skills = [
+  { name: "Product Management", level: 90 },
+  { name: "Business Analysis", level: 85 },
+  { name: "Agile Methodologies", level: 95 },
+  { name: "Generative AI Tools", level: 80 },
+  { name: "Training & Facilitation", level: 85 }
+];
